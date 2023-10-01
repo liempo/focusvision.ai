9bd048b0fb1157df41b8b470d1d0af16c07e8835
Liempo <gonzalesalec@gmail.com>
2023-10-02 00:34:00 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+</script>
+
+<main>
+	<div class="flex bg-brand-background h-screen">
+		<div class="mx-12 my-auto w-1/2">
+			<div
+				class="text-5xl py-12 text-center select-none flex items-center justify-start gap-1"
+			>
+				<div class="p-1 font-extrabold bg-brand-primary text-white">FOCUS</div>
+				<div class="text-brand-primary-dark">VISION</div>
+			</div>
+
+			<p class="text-lg w-2/3">
+				A video conferencing application that uses AI to help you focus on the
+				meeting.
+			</p>
+
+			<div class="flex items-center justify-start space-x-4 mt-8">
+				<button class="px-4 py-2 bg-brand-primary text-white rounded-lg">
+					New meeting
+				</button>
+				<p class="font-sans">or</p>
+
+				<div>
+					<input
+						type="text"
+						class="px-4 py-2 border border-gray-300 rounded-lg"
+						placeholder="Enter a code or a link"
+					/>
+					<button class="px-4 py-2 bg-brand-primary text-white rounded-lg">
+						Join
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="m-auto w-1/2">
+			<!-- Insert lottie here -->
+		</div>
+	</div>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['DM Sans', 'sans-serif']
+			},
+			colors: {
+				brand: {
+					background: '#F6F8FE',
+					primary: '#0a2463',
+					accent: ' #3e92cc'
+				}
+			}
+		}
+	}
+}
