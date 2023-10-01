47185760e6dc136f38b3ad5568c64f60a819ac63
Liempo <gonzalesalec@gmail.com>
2023-10-02 01:56:30 +0800

    Finish up landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player'
 </script>
 
 <main>
-	<div class="flex bg-brand-background h-screen">
-		<div class="mx-12 my-auto w-1/2">
+	<div class="flex bg-brand-background h-screen items-center p-12">
+		<div class="w-full md:w-1/2">
 			<div
 				class="text-5xl py-12 text-center select-none flex items-center justify-start gap-1"
 			>
 				<div class="p-1 font-extrabold bg-brand-primary text-white">FOCUS</div>
 				<div class="text-brand-primary-dark">VISION</div>
 			</div>
-
 			<p class="text-lg w-2/3">
 				A video conferencing application that uses AI to help you focus on the
 				meeting.
 			</p>
-
 			<div class="flex items-center justify-start space-x-4 mt-8">
 				<button class="px-4 py-2 bg-brand-primary text-white rounded-lg">
 					New meeting
 				</button>
 				<p class="font-sans">or</p>
-
 				<div>
 					<input
 						type="text"
@@ -34,8 +32,17 @@
 				</div>
 			</div>
 		</div>
-		<div class="m-auto w-1/2">
-			<!-- Insert lottie here -->
+		<div class="p-8 hidden md:block">
+			<LottiePlayer
+				background="transparent"
+				width={500}
+				height={500}
+				controls={false}
+				controlsLayout={[]}
+				autoplay
+				loop
+				src="/lottie/hero.json"
+			/>
 		</div>
 	</div>
 </main>
