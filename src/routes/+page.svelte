da591bf98a60175be695ba5b40c5dc2f8cdd5eda
Liempo <gonzalesalec@gmail.com>
2023-10-04 23:41:14 +0800

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<div class="flex bg-brand-background h-screen items-center p-12">
-		<div class="w-full md:w-1/2">
+	<div class="flex bg-brand-background h-screen items-center justify-center p-12">
+		<div class="p-8">
 			<div
 				class="text-5xl py-12 text-center select-none flex items-center justify-start gap-1"
 			>
@@ -15,7 +15,7 @@
 				A video conferencing application that uses AI to help you focus on the
 				meeting.
 			</p>
-			<div class="flex items-center justify-start space-x-4 mt-8">
+			<div class="flex flex-col md:flex-row items-start md:items-center justify-start gap-4 mt-8">
 				<button class="px-4 py-2 bg-brand-primary text-white rounded-lg">
 					New meeting
 				</button>
@@ -23,10 +23,10 @@
 				<div>
 					<input
 						type="text"
-						class="px-4 py-2 border border-gray-300 rounded-lg"
-						placeholder="Enter a code or a link"
+						class="px-4 py-2 border border-gray-300 rounded-lg w-2/3"
+						placeholder="Enter meeting code"
 					/>
-					<button class="px-4 py-2 bg-brand-primary text-white rounded-lg">
+					<button class="px-4 py-2 bg-brand-primary text-white rounded-lg hover:bg-brand-accent">
 						Join
 					</button>
 				</div>
