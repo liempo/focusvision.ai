5a30b2e5b98d6f8d9105b8c8b8df9757224d1ffe
Liempo <gonzalesalec@gmail.com>
2023-10-08 13:58:30 +0800

    Update join page and add slug handle

--- src/routes/join/[slug]/+page.svelte
+++ src/routes/join/[slug]/+page.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-	import Banner from '@/components/banner.svelte'
 	import { onMount } from 'svelte'
+	import Banner from '@/components/banner.svelte'
+
+	import type { PageData } from './$types'
+	export let data: PageData
 
 	let stream: MediaStream
 	let videoRef: HTMLVideoElement
 
-	let joinState = {
+	let profile = {
 		name: '',
-		isAudioRecording: false,
-		isWebcamRecording: true
+		audio: true,
+		video: true
 	}
 
 	const getStream = async () => {
 		try {
 			stream = await navigator.mediaDevices.getUserMedia({
 				video: true,
-				audio: false
+				audio: true
 			})
 			videoRef.srcObject = stream
 		} catch (err) {
@@ -35,6 +38,10 @@
 	})
 </script>
 
+<svelte:head>
+	<title>FocusVision | {data.meetingCode}</title>
+</svelte:head>
+
 <main class="flex h-screen flex-col bg-brand-background">
 	<header class="flex items-center justify-between px-4 py-2">
 		<title class="flex items-center justify-start space-x-4 text-lg">
@@ -50,25 +57,37 @@
 			</p>
 		</div>
 
-		<video
-			class={` my-4 rounded-2xl shadow-xl ${
-				stream ? 'animate-grow' : 'hidden'
-			}`}
-			width="640"
-			height="480"
-			autoplay={true}
-			bind:this={videoRef}
-		>
-			<track kind="captions" />
-		</video>
+		<div class={`relative my-4 ${stream ? 'animate-grow' : 'hidden'}`}>
+			<video
+				class={`rounded-2xl shadow-xl ${!profile.video && 'opacity-0'}`}
+				width="640"
+				height="480"
+				autoplay={true}
+				muted={true}
+				bind:this={videoRef}
+			>
+				<track kind="captions" />
+			</video>
 
+			{#if !profile.video}
+				<div
+					class="absolute left-0 top-0 flex h-full w-full rounded-2xl bg-white shadow-sm"
+				>
+					<div class="m-auto h-32 w-32 rounded-full bg-brand-accent p-10">
+						<p class="text-center font-sans text-5xl text-white">
+							{profile.name[0] ?? ''}
+						</p>
+					</div>
+				</div>
+			{/if}
+		</div>
 		<div
 			class={`flex w-full items-center justify-between ${
 				stream ? 'animate-slide' : 'hidden'
 			}`}
 		>
 			<input
-				bind:value={joinState.name}
+				bind:value={profile.name}
 				type="text"
 				class="w-1/2 rounded-lg border border-gray-300 px-4 py-2"
 				placeholder="Enter your name"
@@ -77,28 +96,49 @@
 			<div class="grid grid-flow-col gap-2">
 				<button
 					on:click={() => {
-						joinState.isAudioRecording = !joinState.isAudioRecording
+						profile.audio = !profile.audio
 					}}
 					class="rounded-lg bg-brand-primary px-4 py-2 text-white hover:bg-brand-accent"
 				>
-					{#if joinState.isAudioRecording}
+					{#if profile.audio}
 						<img
 							class="h-6 w-6 bg-transparent"
-							src="/icons/mic_muted.svg"
+							src="/icons/mic_on.svg"
 							alt="Mic muted"
 						/>
 					{:else}
 						<img
 							class="h-6 w-6 bg-transparent"
-							src="/icons/mic_listening.svg"
+							src="/icons/mic_off.svg"
 							alt="Mic on"
 						/>
 					{/if}
 				</button>
 
 				<button
+					on:click={() => {
+						profile.video = !profile.video
+					}}
+					class="rounded-lg bg-brand-primary px-4 py-2 text-white hover:bg-brand-accent"
+				>
+					{#if profile.video}
+						<img
+							class="h-6 w-6 bg-transparent"
+							src="/icons/video_on.svg"
+							alt="Video off"
+						/>
+					{:else}
+						<img
+							class="h-6 w-6 bg-transparent"
+							src="/icons/video_off.svg"
+							alt="Video on"
+						/>
+					{/if}
+				</button>
+
+				<button
 					class="rounded-lg bg-brand-primary px-4 py-2 text-white hover:bg-brand-accent disabled:bg-gray-500"
-					disabled={!joinState.name}
+					disabled={!profile.name}
 				>
 					Join
 				</button>
